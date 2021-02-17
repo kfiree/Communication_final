54fb44e2efcfc968e1c499e7bda6baacf6c3cad8
kfiree <74012686+kfiree@users.noreply.github.com>
2021-02-17 13:01:24 +0200

    Update Q1.py
    add main interface for q1 and start q4

--- Communication_final/Q1/Q1.py
+++ Communication_final/Q1/Q1.py
@@ -8,22 +8,21 @@ def print_pkt(pkt):
 # ----- Task 1.1: Sniffing Packets -----
 
 def q1A_B():
+    print("----- Task 1.1: Sniffing Packets -----")
     f1 = 'icmp'
     f2 = 'tcp and dst port 23 and src host 10.9.0.5'
     f3 = 'net 128.230.0.0/16'
 
     pkt1 = sniff(filter=f1, prn=print_pkt)
-    pkt1.show()
     pkt2 = sniff(filter=f2, prn=print_pkt)
-    pkt2.show()
     pkt3 = sniff(filter=f3, prn=print_pkt)
-    pkt3.show()
 
 
 # ----- Task 1.2: Spoofing ICMP Packets -----
 
-def q2A():
-    ip = IP(src='1.2.3.4', dst='')  # add dst
+def q2():
+    print("----- Task 1.2: Spoofing ICMP Packets -----")
+    ip = IP(src='1.2.3.4', dst = '10.0.2.7')  # add dest ( send it to another VM in the same network)
     icmp = ICMP()
     pkt = ip / icmp
     pkt.show()
@@ -48,7 +47,7 @@ def q3():
             print('error. no reply received')
             break
         elif reply.type == 3:
-            print("we have reached our destination")
+            print("we have reached our destination, and its only took ", i, "routes")
             messageReceived = True
         else:
             print("message have been dropped by", reply.src)
@@ -69,3 +68,9 @@ def spoof(pkt):
 
 def q4():
     pkt = sniff(filter='icmp', prn=spoof)
+
+
+
+pointersDict = {1:q1A_B, 2:q2, 3:q3, 4:q4}
+taskNum= input("choose task : \n task number 1.")
+task = pointersDict.get(int(taskNum))()
